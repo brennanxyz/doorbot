c0903ccb43e6d158e318c5b2fce3bd015022f27e
Brennan Harris <brennan@brennanharris.xyz>
2023-08-25 17:50:42 -0600

    WiFi tested

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,105 @@
-use std::{
-    thread::sleep,
-    time::Duration,
-};
+use std::{thread::sleep, time::Duration};
 
-use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use esp_idf_hal::{
-    peripherals::Peripherals,
-};
-use esp_idf_svc::{
-    wifi::EspWifi,
-    nvs::EspDefaultNvsPartition,
-    eventloop::EspSystemEventLoop,
-}
-use embedded_svc::wifi::{ClientConfiguration, Wifi, Configuration};
+use embedded_svc::wifi::{ClientConfiguration, Configuration, Wifi};
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, wifi::EspWifi};
+use esp_idf_sys as _;
 use log::*;
 
 fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_sys::link_patches();
-    // Bind the log crate to the ESP Logging facilities
+    esp_idf_sys::link_patches(); // don't remove
     esp_idf_svc::log::EspLogger::initialize_default();
+    info!("Patches linked. Main loop entered.");
+    let peripherals = match Peripherals::take() {
+        Some(p) => {
+            info!("Peripherals taken.");
+            p
+        }
+        None => {
+            error!("Peripheral taking FAILED");
+            panic!();
+        }
+    };
 
-    info!("Hello, world!");
+    let sys_loop = match EspSystemEventLoop::take() {
+        Ok(sl) => {
+            info!("Event loop taken.");
+            sl
+        }
+        Err(e) => {
+            error!("Event loop taking FAILED | {}", e);
+            panic!();
+        }
+    };
+
+    let nvs = match EspDefaultNvsPartition::take() {
+        Ok(part) => {
+            info!("Partition taken.");
+            part
+        }
+        Err(e) => {
+            error!("Partition taking FAILED | {}", e);
+            panic!();
+        }
+    };
+
+    let mut wifi_driver = match EspWifi::new(peripherals.modem, sys_loop, Some(nvs)) {
+        Ok(wfd) => {
+            info!("WiFi driver made.");
+            wfd
+        }
+        Err(e) => {
+            error!("WiFi driver FAILED | {}", e);
+            panic!();
+        }
+    };
+
+    match wifi_driver.set_configuration(&Configuration::Client(ClientConfiguration {
+        ssid: "".into(), // TODO: replace with env vars
+        password: "".into(),
+        ..Default::default()
+    })) {
+        Ok(_) => {
+            info!("WiFi driver configured.");
+        }
+        Err(e) => {
+            error!("WiFi driver configuration FAILED | {}", e);
+            panic!();
+        }
+    };
+
+    match wifi_driver.start() {
+        Ok(_) => {
+            info!("WiFi driver started.");
+        }
+        Err(e) => {
+            error!("WiFi driver start FAILED | {}", e);
+            panic!();
+        }
+    };
+
+    match wifi_driver.connect() {
+        Ok(_) => {
+            info!("WiFi driver initiated connection.");
+        }
+        Err(e) => {
+            error!("WiFi driver connection initiation FAILED | {}", e);
+            panic!();
+        }
+    };
+
+    while !wifi_driver.is_connected().unwrap() {
+        let config = wifi_driver.get_configuration().unwrap();
+        warn!("Waiting for station {:?}...", config);
+    }
+
+    info!("Connected.");
+
+    loop {
+        println!(
+            "IP info: {:?}",
+            wifi_driver.sta_netif().get_ip_info().unwrap()
+        );
+        sleep(Duration::new(15, 0));
+    }
 }
